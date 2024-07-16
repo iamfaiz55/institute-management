fd05eab185e9d712c792b1d044453e88d463d240
john <you@example.com>
2024-07-16 15:31:47 +0530

    link

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.static("uploads"))
 app.use(express.static("public"))
 app.use(cors({
-    origin: "*",
+    origin: "https://institute-management-oa5o.onrender.com",
     credentials:true
 }))
 app.use(cookieparser())
