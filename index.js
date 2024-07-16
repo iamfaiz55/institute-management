a51f4a0f6a6a6b1f3aeb379f3d036b3b09c5920f
john <you@example.com>
2024-07-16 15:13:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const cookieparser = require("cookie-parser")
+const { adminProtected } = require("./middleware/adminProtected")
+// const { adminProtected } =   require("./middleware/adminProtected")
+require("dotenv").config()
+
+mongoose.connect(process.env.MONGO_URL)
+const app = express()
+app.use(express.json())
+app.use(express.static("uploads"))
+app.use(express.static("public"))
+app.use(cors({
+    origin: "*",
+    credentials:true
+}))
+app.use(cookieparser())
+
+
+app.use("/api/skillhub/student", require("./routes/user.routes"))
+app.use("/api/skillhub/auth", require("./routes/admin.auth.route"))
+app.use("/api/skillhub",adminProtected, require("./routes/admin.routes"))
+
+app.use("*", (req, res)=> {
+    res.status(404).json({message:"Resoure Not Found"})
+})
+
+app.use((err, req, res, next)=>{
+    console.log(err);
+    res.status(500).json({message:"Something went wrong"})
+})
+
+mongoose.connection.once("open", ()=>{
+    console.log("MONGO CONNECTED");
+    app.listen(process.env.PORT, console.log("SERVER RUNNING"))
+})
